f9d8e7fc8bb6cdfd94f2f5941c5880230a3a8294
serbio18 <serbio18@yahoo.es>
2022-02-12 17:14:43 -0500

    A program that prints all single digit numbers of base 10 starting from 0, followed by a new line. You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line, except q and e
+ * Return: Aways 0 (Sucess)
+ */
+int main(void)
+{
+	int ch = 0;
+	while (ch < 10)
+	{
+	putchar(ch+'0');
+	ch++;
+	}
+	putchar('\n');
+	return (0);
+}
