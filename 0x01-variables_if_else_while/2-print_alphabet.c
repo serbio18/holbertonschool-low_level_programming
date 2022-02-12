5e4a77f7566a2e9eee0cf030375a4e6c2b3ac76d
serbio18 <serbio18@yahoo.es>
2022-02-12 14:13:58 -0500

    A program that prints the alphabet in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *  * main - prints random number: positive, negative or zero
+ *   * Rerturns: Always 0
+ *    */
+int main(void)
+{
+       	char ch;
+
+	for(ch = 'a' ; ch <= 'z' ; ch++)
+       	{
+        putchar(ch);
+	}		       }
+        
+	return(0);
+}
