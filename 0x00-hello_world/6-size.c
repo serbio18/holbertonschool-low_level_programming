d2f541365f0a0e46687f7ede4c8de78480d520da
serbio18 <serbio18@yahoo.es>
2022-02-13 13:20:35 -0500

    A program that prints the size of various types on the computer it is compiled and run on.

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.hi>
+/**
+ *main - init
+ *Return: Always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of char: %d byte(s)", sizeof(char));
+	return (0);
+}
