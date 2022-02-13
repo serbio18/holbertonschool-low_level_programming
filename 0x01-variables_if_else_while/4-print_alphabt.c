d7ae023544ad14f1d34099dc28cd709889c50bc3
serbio18 <serbio18@yahoo.es>
2022-02-13 17:54:12 -0500

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-       	char ch;
+	char ch;
 
 	for (ch = 'a' ; ch <= 'z' ; ch++)
        	{
-	if (ch != 'e' && ch != 'q')
+		if (ch != 'e' && ch != 'q')
 	{
 	putchar(ch);
 	}
