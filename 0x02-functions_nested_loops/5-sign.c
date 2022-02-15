556c38c7f97f22b284662289deb1f64a07a7a81a
serbio18 <serbio18@yahoo.es>
2022-02-15 18:39:16 -0500

    A function that prints the sign of a number.

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,25 +3,25 @@
 #include <ctype.h>
 
 /**
- * main - prints the alphabet in lowercase,
+ * print_sign - prints the alphabet in lowercase,
  * followed by a new line, except q and e
- * Return: Aways 0 or 1 (Sucess)
+ * Return: Aways 0 or 1 or -1 (Sucess)
  */
 int print_sign(int n)
 {
 	if (n > 0)
-	{	
-		_putchar('+');
-	       	return (1);
+	{
+	_putchar('+');
+	return (1);
        	}
 	else if (n < 0)
-	{	
-		_putchar('-');
-		 return (-1);
+	{
+	_putchar('-');
+	return (-1);
 	}
-       	else
-       	{
-		_putchar('0');
-		 return (0);
+	else
+	{
+	_putchar('0');
+	return (0);
 	}
 }
