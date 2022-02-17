2a67ee4b2cbb636fbd32803d64ad24b5aac914c1
serbio18 <serbio18@yahoo.es>
2022-02-17 10:00:58 -0500

    This file Write a function that checks for uppercase character.

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_remaining_days - takes a date and prints how many days are
+ * left in the year, taking leap years into account
+ * @month: month in number format
+ */
+int _isupper(int c)
+{
+	if(isupper(c))
+		return(1);
+	else
+		return(0);
+}
