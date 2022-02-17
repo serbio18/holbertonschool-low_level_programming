c884ffc801b0a664a9cd6af7d7bb2190561235bd
serbio18 <serbio18@yahoo.es>
2022-02-17 14:21:44 -0500

    A file with a function that multiplies two integers.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+
+/**
+ *mul - function that multiplies two integers..
+ * @a: the first integer
+ * @b: the second integer
+ * Return: the result of the multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
