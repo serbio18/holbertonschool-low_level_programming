b8cd28d73ebc60da28a9a96eaf2fe63ecd58f768
serbio18 <serbio18@yahoo.es>
2022-02-17 23:38:30 -0500

    Add void print_diagonal(int n); proto

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -16,11 +16,13 @@ void print_diagonal(int n)
 
 	for (i = 0; i < n; i++)
 	{
+		/*if (i <= 0)
+		_putchar('\n');*/
 		if (i > 0)
 		_putchar('\\');
 		_putchar('\n');
 
-		for (j = 0; j < i; j++)
+		for (j = 1; j <= i; j++)
 		{
 			_putchar(' ');
 		}
