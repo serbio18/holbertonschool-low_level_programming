2ee654d40f3fc6a216693ccab6688408e74bf73e
serbio18 <serbio18@yahoo.es>
2022-02-19 01:10:51 -0500

    A program that FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,11 +14,11 @@ int main(void)
 	for (i = 1; i <= size; i++)
 	{
 		if ( i % 15 == 0)
-			printf(" FizzBuzz ");
-		if ( i % 3 == 0)
-			printf(" Fizz ");
+			printf("FizzBuzz ");
+		else if ( i % 3 == 0)
+			printf("Fizz ");
 		else if ( i % 5 == 0)
-			printf(" Buzz ");
+			printf("Buzz ");
 		else
 			printf("%d ", i);
 	}
