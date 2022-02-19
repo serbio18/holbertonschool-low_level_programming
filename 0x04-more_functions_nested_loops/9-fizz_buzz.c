9a44f52b16bd53c6998b1e822f2ca948d07b5182
serbio18 <serbio18@yahoo.es>
2022-02-19 01:47:12 -0500

    A program that FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,21 +3,22 @@
 /**
  * main - function that  prints a square, followed by a new line.
  *
- * Return: void
+ * Return: always 0
  */
 
 int main(void)
 {
 	int size, i;
+
 	size = 100;
 
 	for (i = 1; i <= size; i++)
 	{
-		if ( i % 15 == 0)
+		if (i % 15 == 0)
 			printf("FizzBuzz ");
-		else if ( i % 3 == 0)
+		else if (i % 3 == 0)
 			printf("Fizz ");
-		else if ( i % 5 == 0)
+		else if (i % 5 == 0)
 			printf("Buzz ");
 		else
 			printf("%d ", i);
