68433846a2e065e25ba98d71b1c8926317c778bb
serbio18 <serbio18@yahoo.es>
2022-02-21 18:29:29 -0500

    Add _putchar(c)

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,9 +2,16 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <string.h>
 
 void reset_to_98(int *n);
-
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+int _putchar(char c);
 
 #endif/* MAIN_H */
 
