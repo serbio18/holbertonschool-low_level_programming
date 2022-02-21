5f394f5cd35389394d674def78e37938c79a8da7
serbio18 <serbio18@yahoo.es>
2022-02-21 18:57:16 -0500

    Add file with unction that prints a string, in reverse, followed by a new line.x

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - a function that returns the length of a string.
+ * @s: the string
+ * Return: @void
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	int l = 0;
+	int j;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	l = i;
+	j = l;
+
+	while (j != 0)
+	{
+		_putchar(s[j]);
+		j--;
+	}
+	_putchar('\n');
+}
