c8d48a271d3f4d383fc4100059e6443c53e4f552
serbio18 <serbio18@yahoo.es>
2022-02-22 11:47:42 -0500

    Add file with  a function that  hat prints half of a string, followed by a new line.

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - a function that reverses a string.
+ * @str: the string
+ * Return: @void
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+	int l = 0;
+	int n = 0;
+
+	while (str[l] != '\0')
+	{
+		l++;
+	}
+	if( n%2 ==  0)
+		n = l/2;
+	else 
+		n = (l - 1) / 2;
+
+	i = n;
+
+	while (i < l)
+	{
+		_putchar(str[i]);
+		i ++;
+	}
+	_putchar('\n');
+}
