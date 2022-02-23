1eb64b219ce02753a5f9f3d31eec46decba86493
serbio18 <serbio18@yahoo.es>
2022-02-23 15:11:20 -0500

    Add a file with function that concatenates two strings.

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * *_strcat - a function that that concatenates two strings.
+ * @dest: the
+ * @src: the
+ * Return: @void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int length, j;
+
+  /* store length of s1 in the length variable*/
+  length = 0;
+  while (dest[length] != '\0') {
+    ++length;
+  }
+
+  /* concatenate s2 to s1*/
+  for (j = 0; src[j] != '\0'; ++j, ++length) {
+    dest[length] = src[j];
+  }
+
+  /* terminating the s1 string*/
+  dest[length] = '\0';
+
+  puts(dest);
+
+
+	return(0);
+}
