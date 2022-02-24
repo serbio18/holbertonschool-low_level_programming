0694dd4b6002931bbbefe4d1fd4e37c3653f7451
serbio18 <serbio18@yahoo.es>
2022-02-24 10:53:31 -0500

    Add a file with function that concatenates two strings.

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,23 +10,23 @@
  */
 char *_strcat(char *dest, char *src)
 {
-  /* store length of s1 in the length variable*/
-  int l1 = 0;
-  int l2= 0;
-  int j;
+	/* store length of s1 in the length variable*/
+	int l1 = 0;
+	int l2 = 0;
+	int j;
 
-  while (dest[l1] != '\0') 
-  {
-	  l1++;
-  }
-  while (src[l2] != '\0')
-  {
-	  l2++;
-  }
-
-  /* concatenate s2 to s1*/
-  for (j = 0; j <= l2; j++) {
-    dest[l1] = src[j];
-  }
-  return (dest);
+	while (dest[l1] != '\0')
+	{
+		l1++;
+	}
+	while (src[l2] != '\0')
+	{
+		l2++;
+	}
+	/* concatenate s2 to s1*/
+	for (j = 0; j <= l2; j++)
+	{
+		dest[l1] = src[j];
+	}
+	return (dest);
 }
