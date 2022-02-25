ba8d2941b508a4f750ac76e511dac1a0f117dc49
serbio18 <serbio18@yahoo.es>
2022-02-25 12:06:20 -0500

    Add a file with a that copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,18 +12,17 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	/* store length of s1 in the length variable*/
-	int l1 = 0;
-	int l2 = 0;
+	int  l1 = 0;
+	int  l2 = 0;
 	int j;
+
+	printf("%d\n%d\n", l1, l2);
 
 	while (src[l2] != '\0')
 	{
-		l2++;
+			l2++;
 	}
-	l2 = -1;
-	/* concatenate s2 to s1*/
-	for (j = 0; j < n ; j++)
+	for (j = 0; j <= n && j <= l2; j++)
 	{
 		dest[l1] = src[j];
 		l1++;
